fa1eb36e87ddb17506a592595c8161805ea200a0
Evan Liomain <evan.liomain@ext.viragegroup.com>
2024-09-30 14:17:05 +0200

    ✨ Add outline/background features

--- src/output-tools.js
+++ src/output-tools.js
@@ -1,12 +1,14 @@
+import "./output-tools.css";
 import { doAsync } from "./utils/do-async";
 import { htmlToElement } from "./utils/html-to-element";
-import './output-tools.css';
 
 document
   .querySelector(".container__item--output .hstack .header__title")
   .remove();
 addCompareOption();
 addGridOption();
+addOutlineOption();
+addBackgroundOption();
 
 doAsync(unCheckSlideNCompare)();
 doAsync(displayDiff)();
@@ -23,6 +25,150 @@ function displayGrid() {
   const target = document.getElementById("overlay-grid");
   const opacity = target.attributeStyleMap.get("opacity").value;
   target.attributeStyleMap.set("opacity", 1 === opacity ? 0 : 1);
+}
+
+function createOutline() {
+  const iframe = document.querySelector("iframe");
+  const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+  const observer = new MutationObserver(() => {
+    if (iframe.classList.contains("display-ouline")) {
+      injectOutlineStyle();
+    }
+  });
+
+  // Commence à observer le noeud cible pour les mutations précédemment configurées
+  observer.observe(iframeDoc, { attributes: true, childList: true });
+}
+
+function createBackground() {
+  const iframe = document.querySelector("iframe");
+  const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+  const observer = new MutationObserver(() => {
+    if (iframe.classList.contains("display-background")) {
+      injectBackgroundStyle();
+    }
+  });
+
+  // Commence à observer le noeud cible pour les mutations précédemment configurées
+  observer.observe(iframeDoc, { attributes: true, childList: true });
+}
+
+function displayOutline() {
+  const iframe = document.querySelector("iframe");
+  const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+  if (!iframe.classList.contains("display-ouline")) {
+    iframe.classList.add("display-ouline");
+    injectOutlineStyle();
+  } else {
+    iframe.classList.remove("display-ouline");
+    iframeDoc.head.querySelector("[data-outline]")?.remove();
+  }
+}
+
+function displayBackground() {
+  const iframe = document.querySelector("iframe");
+  const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+  if (!iframe.classList.contains("display-background")) {
+    iframe.classList.add("display-background");
+    injectBackgroundStyle();
+  } else {
+    iframe.classList.remove("display-background");
+    iframeDoc.head.querySelector("[data-background]")?.remove();
+  }
+}
+
+function injectOutlineStyle() {
+  const iframe = document.querySelector("iframe");
+  const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+  // Injecter du CSS dans l'iframe
+  const style = iframeDoc.createElement("style");
+  style.type = "text/css";
+  style.setAttribute("data-outline", 0);
+  iframeDoc.head.appendChild(style);
+  style.setAttribute("data-style", "1");
+  style.innerHTML = `* {
+    position: relative;
+    &:before {
+      content: '';
+      position: absolute;
+      inset: 0;
+      outline: 4px dotted var(--outline-color);
+      outline-offset: -2px;
+      z-index: 10;
+    }
+    --outline-color: red;
+    * {
+      --outline-color: blue;
+      * {
+        --outline-color: green;
+        * {
+          --outline-color: yellow;
+          * {
+            --outline-color: pink;
+            * {
+              --outline-color: aqua;
+              * {
+                --outline-color: blueviolet;
+                * {
+                  --outline-color: brown;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
+}
+
+function injectBackgroundStyle() {
+  const iframe = document.querySelector("iframe");
+  const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+  // Injecter du CSS dans l'iframe
+  const style = iframeDoc.createElement("style");
+  style.type = "text/css";
+  style.setAttribute("data-background", 0);
+  iframeDoc.head.appendChild(style);
+  style.setAttribute("data-style", "1");
+  style.innerHTML = `* {
+    position: relative;
+    &:before {
+      content: '';
+      position: absolute;
+      inset: 0;
+      background-color: var(--outline-color);
+      opacity: 0.4;
+      z-index: 10;
+    }
+    --outline-color: red;
+    * {
+      --outline-color: blue;
+      * {
+        --outline-color: green;
+        * {
+          --outline-color: yellow;
+          * {
+            --outline-color: pink;
+            * {
+              --outline-color: aqua;
+              * {
+                --outline-color: blueviolet;
+                * {
+                  --outline-color: brown;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
 }
 
 function unCheckSlideNCompare() {
@@ -136,6 +282,62 @@ function addGridOption() {
     });
 }
 
+function addOutlineOption() {
+  const template = `
+  <label
+    title="Show outline on every tags of the output"
+    style="display: flex; gap: 0.25rem; align-items: center;"
+  >
+    <input
+      id="output-outline-input"
+      type="checkbox"
+      value="true"
+      />
+    ${outlineIcon()}
+  </label>
+  `;
+
+  document
+    .querySelector(".container__item--output .header__extra-info .hstack")
+    .appendChild(htmlToElement(template));
+
+  createOutline();
+
+  document
+    .getElementById("output-outline-input")
+    .addEventListener("input", (e) => {
+      displayOutline();
+    });
+}
+
+function addBackgroundOption() {
+  const template = `
+  <label
+    title="Show background on every tags of the output"
+    style="display: flex; gap: 0.25rem; align-items: center;"
+  >
+    <input
+      id="output-background-input"
+      type="checkbox"
+      value="true"
+      />
+    ${backgroundIcon()}
+  </label>
+  `;
+
+  document
+    .querySelector(".container__item--output .header__extra-info .hstack")
+    .appendChild(htmlToElement(template));
+
+  createBackground();
+
+  document
+    .getElementById("output-background-input")
+    .addEventListener("input", (e) => {
+      displayBackground();
+    });
+}
+
 function gridIcon() {
   return `
   <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
@@ -150,8 +352,7 @@ function gridIcon() {
 }
 
 function slideNCompareIcon() {
-  return `
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
+  return `<svg xmlns="http://www.w3.org/2000/svg"
     viewBox="10 10 80 80"
     width="20"
   >
@@ -163,8 +364,7 @@ function slideNCompareIcon() {
 }
 
 function diffIcon() {
-  return `
-  <svg xmlns="http://www.w3.org/2000/svg"
+  return `<svg xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 36 36"
     width="20"
   >
@@ -178,8 +378,7 @@ function diffIcon() {
 }
 
 function compareIcon() {
-  return `
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
+  return `<svg xmlns="http://www.w3.org/2000/svg"
     viewBox="-5.0 -10.0 110 110"
     width="20"
   >
@@ -205,6 +404,44 @@ function compareIcon() {
       stroke-width="5"
       stroke-linecap="round"
       stroke-linejoin="round"
+    />
+  </svg>`;
+}
+
+function outlineIcon() {
+  return `<svg xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 36 36"
+    width="20"
+  >
+    <rect
+      x="10"
+      y="10"
+      width="15" height="15"
+      stroke="currentColor"
+      stroke-width="2"
+      fill="transparent"
+      stroke-linejoin="round"
+    />
+    <rect
+      x="5"
+      y="5"
+      width="25" height="25"
+      fill="transparent"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-dasharray=".2 4"
+      stroke-linecap="round"
+    />
+  </svg>`;
+}
+function backgroundIcon() {
+  return `<svg xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 64 64"
+    width="20"
+  >
+    <path
+      d="M24.02,23h15.96c0.56,0,1.02,0.46,1.02,1.02l0,15.96c0,0.56-0.46,1.02-1.02,1.02H24.02C23.46,41,23,40.54,23,39.98V24.02  C23,23.46,23.46,23,24.02,23z M5,57.98V24.02C5,23.46,5.46,23,6.02,23h13.96c0.56,0,1.02,0.46,1.02,1.02v15.96  c0,1.67,1.35,3.02,3.02,3.02h15.96c0.56,0,1.02,0.46,1.02,1.02l0,13.96c0,0.56-0.46,1.02-1.02,1.02H6.02C5.46,59,5,58.54,5,57.98z   M43,39.98V24.02c0-1.67-1.35-3.02-3.02-3.02H24.02C23.46,21,23,20.54,23,19.98V6.02C23,5.46,23.46,5,24.02,5h33.96  C58.54,5,59,5.46,59,6.02l0,33.96c0,0.56-0.46,1.02-1.02,1.02H44.02C43.46,41,43,40.54,43,39.98z"
+      fill="currentColor"
     />
   </svg>`;
 }
