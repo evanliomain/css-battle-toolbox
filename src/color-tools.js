38284f19b80adf50aacf64a82130d30c47bcb500
Evan Liomain <evan.liomain@ext.viragegroup.com>
2024-10-02 14:27:34 +0200

    ✨ [color tool] Improve color tool to match better colors
    (sames as @beo color mixer tool)

--- src/color-tools.js
+++ src/color-tools.js
@@ -1,3 +1,4 @@
+import "./color-tools.css";
 import { doAsync } from "./utils/do-async";
 import { htmlToElement } from "./utils/html-to-element";
 
@@ -23,13 +24,26 @@ function template() {
     <div class="input-container">
       <label for="color-input-background">Background color</label>
       <input id="color-input-background" type="text" class="js-color-input-minify" placeholder="#FFFFFF" />
-      <div style="display: grid; gap: 0.1rem; grid-template-columns: repeat(6, 1fr);">
-      ${colors.map((color) => `
+      <div class="bullet-buttons">
         <button
           type="button"
-          aria-label="Copy color ${color}"
-          class="colors-list__color js-trigger-color"
-          style="--color: ${color}; font-size: .6em; padding: 0"
+          aria-label="Reset background"
+          data-transition="true"
+          data-hint="Reset background"
+          class="bullet-color-button hint--top-right bullet-reset-button js-trigger-color"
+          onclick="document.getElementById('color-input-background').value = ''"
+          style="--color: var(--clr-bg);"
+        >
+        </button>
+      ${colors
+        .map(
+          (color) => `
+        <button
+          type="button"
+          aria-label="Copy color ${color} as background"
+          data-hint="Copy color ${color} as background"
+          class="bullet-color-button hint--top-right js-trigger-color"
+          style="--color: ${color};"
           onclick="document.getElementById('color-input-background').value = '${color}'"
         >
         </button>
@@ -41,13 +55,17 @@ function template() {
     <div class="input-container">
       <label for="color-input-target">Target color</label>
       <input id="color-input-target" type="text" class="js-color-input-minify" />
-      <div style="display: grid; gap: 1rem; grid-template-columns: repeat(6, 1fr);">
-      ${colors.map((color) => `
+      <div class="bullet-buttons">
+      ${colors
+        .map(
+          (color) => `
         <button
           type="button"
-          aria-label="Copy color ${color}"
-          class="colors-list__color js-trigger-color"
-          style="--color: ${color}; font-size: .6em; padding: 0"
+
+          aria-label="Copy color ${color} as foreground"
+          data-hint="Copy color ${color} as foreground"
+          class="bullet-color-button hint--top-left js-trigger-color"
+          style="--color: ${color};"
           onclick="document.getElementById('color-input-target').value = '${color}'"
         >
         </button>
@@ -56,16 +74,16 @@ function template() {
         .join("")}
       </div>
     </div>
-    <button
+    <div
       id="color-result"
-      type="button"
-      class="colors-list__color js-target-color"
-      style="opacity: 0; width: fit-content;"
-      onclick="navigator.clipboard.writeText(this.dataset.color.toUpperCase())"
-    >
-    </button>
-    </div>
-  `;
+      style="
+        grid-column-start: span 2;
+        display: flex; flex-direction: row;
+        flex-wrap: wrap;
+        gap: 8px;
+      "
+    ></div>
+  </div>`;
 }
 
 function addListener() {
@@ -90,22 +108,36 @@ function computeColor() {
     document.getElementById("color-input-background").value || "#ffffff";
   const target = document.getElementById("color-input-target").value;
   const result = document.getElementById("color-result");
-  if ("" === target) {
-    result.style.setProperty("opacity", "0");
+  result.replaceChildren();
+
+  if ("" === target || background === target) {
     return;
   }
   const colors = findColors(background, target);
   if (0 === colors.length) {
-    result.innerText = "No shorter color found";
-    result.style.setProperty("--color", "transparent");
-    result.style.setProperty("opacity", "1");
     return;
   }
-  const [color, err, mix] = colors[0];
-  result.innerText = `${color} (${err.toFixed(2)})`;
-  result.style.setProperty("--color", mix);
-  result.style.setProperty("opacity", "1");
-  result.dataset.color = color;
+  const minError = colors[0][1];
+  colors
+    .filter(([color, err, mix]) => err <= 1 + minError)
+    .filter((_, index) => index < 10)
+    .forEach(([color, err, mix]) => {
+      result.insertAdjacentElement(
+        "beforeend",
+        htmlToElement(`
+        <button
+          type="button"
+          class="colors-list__color js-target-color"
+          style="width: fit-content; --color: ${mix};"
+          data-color="${color}"
+          onclick="navigator.clipboard.writeText(this.dataset.color.toUpperCase())"
+          title="Click to copy color ${color}, with error from ${target} is ${err}"
+        >
+          <span>${color}</span>
+          <span>(${err.toFixed(0)})</span>
+        </button>`),
+      );
+    });
 }
 
 function findColors(background, target) {
@@ -115,24 +147,22 @@ function findColors(background, target) {
       .match(/.{1,2}/g)
       .map((n) => parseInt(n, 16));
   const colors = [];
-  const [r0, g0, b0] = parseHex(background);
-  const [rt, gt, bt] = parseHex(target);
-  const threshold = 7;
+  const bg = parseHex(background);
+  const fg = parseHex(target);
+  const threshold = 10;
 
   for (let r = 0; r < 256; r += 17) {
     for (let g = 0; g < 256; g += 17) {
       for (let b = 0; b < 256; b += 17) {
         for (let a = 0; a < 256; a += 17) {
-          const r1 = (r0 * (255 - a)) / 255 + (r * a) / 255;
-          const g1 = (g0 * (255 - a)) / 255 + (g * a) / 255;
-          const b1 = (b0 * (255 - a)) / 255 + (b * a) / 255;
-          const err = Math.hypot(r1 - rt, g1 - gt, b1 - bt);
+          const mix = mixColors(bg, [r, g, b], a / 255);
+          const rdiff = fg[0] - mix[0];
+          const gdiff = fg[1] - mix[1];
+          const bdiff = fg[2] - mix[2];
+          const err = Math.abs(rdiff) + Math.abs(gdiff) + Math.abs(bdiff);
+
           if (err < threshold) {
-            colors.push([
-              toHex([r, g, b, a]),
-              err,
-              toHex(mixColors([r0, g0, b0], [r, g, b], a / 255)),
-            ]);
+            colors.push([toHex([r, g, b, a]), err, toHex(mix)]);
           }
         }
       }
@@ -155,6 +185,7 @@ function toHex([r, g, b, a]) {
     Math.round(number / 17)
       .toString(16)
       .padStart(1, "0");
+
   let color = `#${hex(r)}${hex(g)}${hex(b)}`;
   const alpha = hex(a ?? 255);
   if ("f" !== alpha) {
